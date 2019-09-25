10373d5f2d20648912fc01b79cc9f9389e9fd1e3
fanjc <fanjc@mail.taiji.com.cn>
2019-09-25 11:10:41 +0800

    oauth2脚本

--- oauth2-request-token.sh
+++ oauth2-request-token.sh
@@ -6,25 +6,26 @@ function getUserInfo(){
 	scope="all"
 	client_secret="123456"
 	redirect_uri="http%3A%2F%2Flocalhost%3A8888%2Foauth2Callback"
+	access_token_uri="http://localhost:9999/oauth/token"
 	user_info_uri="http://localhost:9999/me"
 	#根据code请求token
 	result=`curl --user tdfOauth2SSO213:123456 \
-	http://localhost:9999/oauth/token -X POST \
+	$access_token_uri -X POST \
 	-H "Content-Type:application/x-www-form-urlencoded" \
 	-d "client_id=$client_id&grant_type=$grant_type&scope=$scope&redirect_uri=$redirect_uri&client_secret=$client_secret&code=$1" \
 	|grep '\{'`
-	echo 'result:$result'
+	echo "result:$result"
 	if [[ "$result" =~ "error" ]] #判断result是否包含error
 	then
-		echo 'code:$1 fail'
+		echo "code:$1 fail"
 	else
-		echo 'code:$1 success'
+		echo "code:$1 success"
 		access_token=`echo $result |awk -F "," '{print $1}' | awk -F ":" '{print $2}'|sed 's/\"//g'`
 		authorization_head="Authorization:bearer $access_token"
-		echo 'authorization_head:$authorization_head'
+		echo "authorization_head:$authorization_head"
 		#根据token请求用户信息
 		user_info=`curl -H "$authorization_head" -v $user_info_uri`
-		echo 'user_info:$user_info'
+		echo "user_info:$user_info"
 	fi
 }
 getUserInfo $1
